db09052546e8c33566672856d6a50d29df23057a
GuoDaStathaMa <macheng120@gmail.com>
2019-08-01 18:03:45 +0800

    little problem

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selectedTab: 'redTab',
+      selectedTab: 'blueTab',
       hidden: false,
       fullScreen: true
     }
