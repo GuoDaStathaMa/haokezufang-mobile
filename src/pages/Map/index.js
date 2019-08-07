1fa47569340dd3ab1fb0310aa432fa2691b4c873
GuoDaStathaMa <macheng120@gmail.com>
2019-08-07 21:58:47 +0800

    地图选房30%

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getCity } from 'tools'
+import { Toast } from 'antd-mobile'
 import axios from 'axios'
 import NavHeader from 'common/NavHeader'
 import './index.scss'
@@ -8,46 +9,109 @@ const BMap = window.BMap
 class Map extends React.Component {
   async componentDidMount() {
     await this.getCityArea()
-    this.getMapInfo()
-  }
-
-  async getMapInfo() {
-    let { value } = await getCity()
-    const res = await axios.get(`http://localhost:8080/area/map?id=${value}`)
-    const { status, body } = res.data
-    if (status !== 200) return false
-    body.forEach(v => {})
+    // 把map存在组件
+    this.map = new BMap.Map('container')
   }
 
   async getCityArea() {
+    Toast.loading('加载中', 60, null, true)
     let nowCity = await getCity()
-    let map = new BMap.Map('container')
     let myGeo = new BMap.Geocoder()
     myGeo.getPoint(
       nowCity.label,
       point => {
         if (point) {
-          map.centerAndZoom(point, 11)
-          map.addControl(new BMap.NavigationControl())
-          map.addControl(new BMap.ScaleControl())
+          this.map.centerAndZoom(point, 11)
+          this.map.addControl(new BMap.NavigationControl())
+          this.map.addControl(new BMap.ScaleControl())
 
-          let opts = {
-            position: point, // 指定文本标注所在的地理位置
-            offset: new BMap.Size(-100, -30) //设置文本偏移量
-          }
-          let label = new BMap.Label(
-            `<div class="circleBtn">
-              <p class="name">浦东新区</p>
-              <p>2套</p>
-            </div>`,
-            opts
-          ) // 创建文本标注对象
-          map.addOverlay(label)
+          this.getHouseSourse(nowCity.value)
         }
       },
       nowCity.label
     )
   }
+
+  async getHouseSourse(id) {
+    const res = await axios.get(`http://localhost:8080/area/map?id=${id}`)
+    const { status, body } = res.data
+    if (status !== 200) return false
+    const allHouses = body
+    this.renderCircle(allHouses)
+  }
+
+  renderCircle(body) {
+    body.forEach(v => {
+      const point = new BMap.Point(v.coord.longitude, v.coord.latitude)
+      let opts = {
+        position: point, // 指定文本标注所在的地理位置
+        offset: new BMap.Size(-35, -35) //设置文本偏移量
+      }
+      let label = new BMap.Label(
+        `<div class="circleBtn">
+          <p class="name">${v.label}</p>
+          <p>${v.count}套</p>
+        </div>`,
+        opts
+      )
+
+      label.setStyle({
+        cursor: 'pointer',
+        border: '0px solid rgb(255, 0, 0)',
+        padding: '0px',
+        whiteSpace: 'nowrap',
+        fontSize: '12px',
+        color: 'rgb(255, 255, 255)',
+        textAlign: 'center'
+      })
+      // 创建文本标注对象
+      this.map.addOverlay(label)
+      Toast.hide()
+
+      label.addEventListener('click', function() {
+        console.log(v.label)
+      })
+    })
+  }
+
+  // async getMapInfo(map) {
+  //   let { value } = await getCity()
+  //   const res = await axios.get(`http://localhost:8080/area/map?id=${value}`)
+  //   const { status, body } = res.data
+  //   if (status !== 200) return false
+
+  //   body.forEach(v => {
+  //     const point = new BMap.Point(v.coord.longitude, v.coord.latitude)
+  //     let opts = {
+  //       position: point, // 指定文本标注所在的地理位置
+  //       offset: new BMap.Size(-35, -35) //设置文本偏移量
+  //     }
+  //     let label = new BMap.Label(
+  //       `<div class="circleBtn">
+  //         <p class="name">${v.label}</p>
+  //         <p>${v.count}套</p>
+  //       </div>`,
+  //       opts
+  //     )
+
+  //     label.setStyle({
+  //       cursor: 'pointer',
+  //       border: '0px solid rgb(255, 0, 0)',
+  //       padding: '0px',
+  //       whiteSpace: 'nowrap',
+  //       fontSize: '12px',
+  //       color: 'rgb(255, 255, 255)',
+  //       textAlign: 'center'
+  //     })
+  //     // 创建文本标注对象
+  //     map.addOverlay(label)
+  //     Toast.hide()
+
+  //     label.addEventListener('click', function() {
+  //       console.log(v.label)
+  //     })
+  //   })
+  // }
 
   render() {
     return (
